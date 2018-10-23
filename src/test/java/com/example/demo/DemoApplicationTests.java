b5f0c1066d0cf699962da617126bfdde76fce879
Administrator <1341479360@qq.com>
2018-10-23 11:42:58 +0800

    Initial commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+import com.sun.org.apache.regexp.internal.RE;
+import org.apache.logging.log4j.message.ReusableMessage;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.validator.PublicClassValidator;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.xml.ws.RequestWrapper;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+
+    @Test
+    public void contextLoads() {
+
+
+    }
+
+    @ResponseBody
+    @RequestMapping("")
+    public String select(Model model) {
+
+        String i = "32342324234";
+
+        model.addAttribute("name", "zhangsan");
+
+
+        User u = new User();
+        model.addAttribute("user", u);
+        return "index";
+
+
+    }
+
+
+}
---
+++ src/test/java/com/example/demo/User.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+public class User {
+
+    private String name;
+
+    private int sex;
+
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
